ecc12c36513c3e97f5566f32eff327e1c883abe3
Ryo Ota <nwtgck@gmail.com>
2020-01-03 18:39:56 +0900

    copy <project-root>/remote in build.sh

--- web/build.sh
+++ web/build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p public/static/out public/static-extension
 cp -r ../out/ public/static/out/
+cp -r ../remote/ public/static/remote
 
 for ext in vscode-api-tests theme-defaults
 do
@@ -9,7 +10,7 @@ do
 done
 
 (
-	cd public/remote/web
+	cd public/static/remote/web
 	yarn install
 	cp -r node_modules/ modules/
 )
